f6c5dc0b83e213563a30882dfb4b19e1da410409
zhouyuchao <392318709@qq.com>
2019-05-20 19:53:55 +0800

    +

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -53,7 +53,7 @@ class Index extends Backend
         $url = $this->request->get('url', 'index/index');
         if ($this->auth->isLogin()) {
             if($this->request->isPost()){
-                $this->success(__("You've logged in, do not login again"), $url, null, 0);
+                $this->success(__("You've logged in, do not login again"), $url);
             }
             $this->redirect($url);
         }
@@ -92,7 +92,7 @@ class Index extends Backend
             $result = $this->auth->login($username, $password, $keeplogin ? 86400 : 0);
             if ($result === true) {
                 Hook::listen("admin_login_after", $this->request);
-                $this->success(__('Login successful'), $url, ['url' => $url, 'id' => $this->auth->id, 'username' => $username, 'avatar' => $this->auth->avatar], 0);
+                $this->success(__('Login successful'), $url, ['url' => $url, 'id' => $this->auth->id, 'username' => $username, 'avatar' => $this->auth->avatar]);
             } else {
                 $msg = $this->auth->getError();
                 $msg = $msg ? $msg : __('Username or password is incorrect');
