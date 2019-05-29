689e50900df239f385dbe561473be15fc22fec80
zhouyuchao <392318709@qq.com>
2019-05-29 19:21:52 +0800

    +

--- application/api/controller/Common.php
+++ application/api/controller/Common.php
@@ -53,112 +53,115 @@ class Common extends Api
      */
     public function upload()
     {
-        $file = $this->request->file('file');
         $thumb = input('post.thumb', 1);
         $needWater = input('param.type', 0); //0需要水印，1不需要
-        if (empty($file)) {
-            $this->error(__('No file upload or server upload limit exceeded'));
+
+        $files = $this->request->file('');
+        // halt(count($files));
+        if (empty($files)) {
+            $this->error(__('No file upload or server upload limit exceeded'), null, StatusCode::CODE_UPLOAD_ERROR);
         }
+        $data = [];
+        foreach ($files as $file) {
+            //判断是否已经存在附件
+            $sha1 = $file->hash();
 
-        //判断是否已经存在附件
-        $sha1 = $file->hash();
+            $upload = Config::get('upload');
 
-        $upload = Config::get('upload');
+            preg_match('/(\d+)(\w+)/', $upload['maxsize'], $matches);
+            $type = strtolower($matches[2]);
+            $typeDict = ['b' => 0, 'k' => 1, 'kb' => 1, 'm' => 2, 'mb' => 2, 'gb' => 3, 'g' => 3];
+            $size = (int)$upload['maxsize'] * pow(1024, isset($typeDict[$type]) ? $typeDict[$type] : 0);
+            $fileInfo = $file->getInfo();
+            $suffix = strtolower(pathinfo($fileInfo['name'], PATHINFO_EXTENSION));
+            $suffix = $suffix ? $suffix : 'file';
 
-        preg_match('/(\d+)(\w+)/', $upload['maxsize'], $matches);
-        $type = strtolower($matches[2]);
-        $typeDict = ['b' => 0, 'k' => 1, 'kb' => 1, 'm' => 2, 'mb' => 2, 'gb' => 3, 'g' => 3];
-        $size = (int)$upload['maxsize'] * pow(1024, isset($typeDict[$type]) ? $typeDict[$type] : 0);
-        $fileInfo = $file->getInfo();
-        $suffix = strtolower(pathinfo($fileInfo['name'], PATHINFO_EXTENSION));
-        $suffix = $suffix ? $suffix : 'file';
+            $mimetypeArr = explode(',', strtolower($upload['mimetype']));
+            $typeArr = explode('/', $fileInfo['type']);
 
-        $mimetypeArr = explode(',', strtolower($upload['mimetype']));
-        $typeArr = explode('/', $fileInfo['type']);
-
-        //验证文件后缀
-        if ($upload['mimetype'] !== '*' &&
-            (
-                !in_array($suffix, $mimetypeArr)
-                || (stripos($typeArr[0] . '/', $upload['mimetype']) !== false && (!in_array($fileInfo['type'], $mimetypeArr) && !in_array($typeArr[0] . '/*', $mimetypeArr)))
-            )
-        ) {
-            $this->error(__('Uploaded file format is limited'));
-        }
-        $replaceArr = [
-            '{year}'     => date("Y"),
-            '{mon}'      => date("m"),
-            '{day}'      => date("d"),
-            '{hour}'     => date("H"),
-            '{min}'      => date("i"),
-            '{sec}'      => date("s"),
-            '{random}'   => Random::alnum(16),
-            '{random32}' => Random::alnum(32),
-            '{filename}' => $suffix ? substr($fileInfo['name'], 0, strripos($fileInfo['name'], '.')) : $fileInfo['name'],
-            '{suffix}'   => $suffix,
-            '{.suffix}'  => $suffix ? '.' . $suffix : '',
-            '{filemd5}'  => md5_file($fileInfo['tmp_name']),
-        ];
-        $savekey = $upload['savekey'];
-        $savekey = str_replace(array_keys($replaceArr), array_values($replaceArr), $savekey);
-
-        $uploadDir = substr($savekey, 0, strripos($savekey, '/') + 1);
-        $fileName = substr($savekey, strripos($savekey, '/') + 1);
-        //
-        $splInfo = $file->validate(['size' => $size])->move(ROOT_PATH . '/public' . $uploadDir, $fileName);
-        if ($splInfo) {
-            $imagewidth = $imageheight = 0;
-            if (in_array($suffix, ['gif', 'jpg', 'jpeg', 'bmp', 'png', 'swf'])) {
-                $imgInfo = getimagesize($splInfo->getPathname());
-                $imagewidth = isset($imgInfo[0]) ? $imgInfo[0] : $imagewidth;
-                $imageheight = isset($imgInfo[1]) ? $imgInfo[1] : $imageheight;
-                $image = \think\Image::open(ROOT_PATH . '/public' . $uploadDir . $fileName);
-                if($imagewidth < 900){
-                    $waterImage = 'water_logo_70.png';
-                }else if($imagewidth >= 900 && $imagewidth < 2000){
-                    $waterImage = 'water_logo_120.png';
-                }else {
-                    $waterImage = 'water_logo_150.png';
-                }
-                if($needWater == 0){
-                    //需要水印
-                    $image->water('./assets/img/' . $waterImage, \think\Image::WATER_SOUTHEAST, 50)->save(ROOT_PATH . '/public' . $uploadDir . $fileName);
-                }
-                if ($thumb) {
-                    $thumb200Height = $imageheight > $imagewidth ? (200 * $imageheight/$imagewidth) : 200;
-                    $thumb750Height = $imageheight > $imagewidth ? (750 * $imageheight/$imagewidth) : 750;
-                    $thumb_200_name = str_replace('.' . $suffix, '_thumb_200.' . $suffix, $fileName);
-                    $image->thumb(200, $thumb200Height)->save(ROOT_PATH . '/public' . $uploadDir . $thumb_200_name);
-                    $image = \think\Image::open(ROOT_PATH . '/public' . $uploadDir . $fileName);
-                    $thumb_750_name = str_replace('.' . $suffix, '_thumb_750.' . $suffix, $fileName);
-                    $image->thumb(750, $thumb750Height)->save(ROOT_PATH . '/public' . $uploadDir . $thumb_750_name);
-                }
+            //验证文件后缀
+            if ($upload['mimetype'] !== '*' &&
+                (
+                    !in_array($suffix, $mimetypeArr)
+                    || (stripos($typeArr[0] . '/', $upload['mimetype']) !== false && (!in_array($fileInfo['type'], $mimetypeArr) && !in_array($typeArr[0] . '/*', $mimetypeArr)))
+                )
+            ) {
+                $this->error(__('Uploaded file format is limited'));
             }
-            $params = array(
-                'admin_id'    => 0,
-                'user_id'     => (int)$this->auth->id,
-                'filesize'    => $fileInfo['size'],
-                'imagewidth'  => $imagewidth,
-                'imageheight' => $imageheight,
-                'imagetype'   => $suffix,
-                'imageframes' => 0,
-                'mimetype'    => $fileInfo['type'],
-                'url'         => $uploadDir . $splInfo->getSaveName(),
-                'uploadtime'  => time(),
-                'storage'     => 'local',
-                'sha1'        => $sha1,
-            );
-            $attachment = model("attachment");
-            $attachment->data(array_filter($params));
-            $attachment->save();
-            \think\Hook::listen("upload_after", $attachment);
-            $this->success(__('Upload successful'), [
-                'url' => config('image_domain') . $uploadDir . $splInfo->getSaveName()
-            ]);
-        } else {
-            // 上传失败获取错误信息
-            $this->error($file->getError());
+            $replaceArr = [
+                '{year}'     => date("Y"),
+                '{mon}'      => date("m"),
+                '{day}'      => date("d"),
+                '{hour}'     => date("H"),
+                '{min}'      => date("i"),
+                '{sec}'      => date("s"),
+                '{random}'   => Random::alnum(16),
+                '{random32}' => Random::alnum(32),
+                '{filename}' => $suffix ? substr($fileInfo['name'], 0, strripos($fileInfo['name'], '.')) : $fileInfo['name'],
+                '{suffix}'   => $suffix,
+                '{.suffix}'  => $suffix ? '.' . $suffix : '',
+                '{filemd5}'  => md5_file($fileInfo['tmp_name']),
+            ];
+            $savekey = $upload['savekey'];
+            $savekey = str_replace(array_keys($replaceArr), array_values($replaceArr), $savekey);
+
+            $uploadDir = substr($savekey, 0, strripos($savekey, '/') + 1);
+            $fileName = substr($savekey, strripos($savekey, '/') + 1);
+            //
+            $splInfo = $file->validate(['size' => $size])->move(ROOT_PATH . '/public' . $uploadDir, $fileName);
+            if ($splInfo) {
+                $imagewidth = $imageheight = 0;
+                if (in_array($suffix, ['gif', 'jpg', 'jpeg', 'bmp', 'png', 'swf'])) {
+                    $imgInfo = getimagesize($splInfo->getPathname());
+                    $imagewidth = isset($imgInfo[0]) ? $imgInfo[0] : $imagewidth;
+                    $imageheight = isset($imgInfo[1]) ? $imgInfo[1] : $imageheight;
+                    $image = \think\Image::open(ROOT_PATH . '/public' . $uploadDir . $fileName);
+                    if($imagewidth < 900){
+                        $waterImage = 'water_logo_70.png';
+                    }else if($imagewidth >= 900 && $imagewidth < 2000){
+                        $waterImage = 'water_logo_120.png';
+                    }else {
+                        $waterImage = 'water_logo_150.png';
+                    }
+                    if($needWater == 0){
+                        //需要水印
+                        $image->water('./assets/img/' . $waterImage, \think\Image::WATER_SOUTHEAST, 50)->save(ROOT_PATH . '/public' . $uploadDir . $fileName);
+                    }
+                    if ($thumb) {
+                        $thumb200Height = $imageheight > $imagewidth ? (200 * $imageheight/$imagewidth) : 200;
+                        $thumb750Height = $imageheight > $imagewidth ? (750 * $imageheight/$imagewidth) : 750;
+                        $thumb_200_name = str_replace('.' . $suffix, '_thumb_200.' . $suffix, $fileName);
+                        $image->thumb(200, $thumb200Height)->save(ROOT_PATH . '/public' . $uploadDir . $thumb_200_name);
+                        $image = \think\Image::open(ROOT_PATH . '/public' . $uploadDir . $fileName);
+                        $thumb_750_name = str_replace('.' . $suffix, '_thumb_750.' . $suffix, $fileName);
+                        $image->thumb(750, $thumb750Height)->save(ROOT_PATH . '/public' . $uploadDir . $thumb_750_name);
+                    }
+                }
+                $params = array(
+                    'admin_id'    => 0,
+                    'user_id'     => (int)$this->auth->id,
+                    'filesize'    => $fileInfo['size'],
+                    'imagewidth'  => $imagewidth,
+                    'imageheight' => $imageheight,
+                    'imagetype'   => $suffix,
+                    'imageframes' => 0,
+                    'mimetype'    => $fileInfo['type'],
+                    'url'         => $uploadDir . $splInfo->getSaveName(),
+                    'uploadtime'  => time(),
+                    'storage'     => 'local',
+                    'sha1'        => $sha1,
+                );
+                $attachment = model("attachment");
+                $attachment->data(array_filter($params));
+                $attachment->isUpdate(false)->save();
+                \think\Hook::listen("upload_after", $attachment);
+                $data['url'][] = config('image_domain') . $uploadDir . $splInfo->getSaveName();
+            } else {
+                // 上传失败获取错误信息
+                $this->error($file->getError());
+            }
         }
+        $this->success(__('Upload successful'), $data);
     }
 
 }
